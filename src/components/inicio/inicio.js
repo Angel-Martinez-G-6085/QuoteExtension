f5cc5c2ec49f4df05b5c55a958fc2256eeceac00
Angel Ivan Martinez Garrido <mgangelivan@gmail.com>
2022-03-26 23:01:11 -0600

    función para colocar la información llamada en inicio

--- src/components/inicio/inicio.js
+++ src/components/inicio/inicio.js
@@ -1,8 +1,10 @@
 import './inicio.scss';
 import INICIO from './inicio.html';
+import { setData } from '../../utils/setData';
 
 const loadInicio = () => {
   const header = document.querySelector('.header');
   header.insertAdjacentHTML('afterend', INICIO);
+  setData();
 };
 export default loadInicio;
